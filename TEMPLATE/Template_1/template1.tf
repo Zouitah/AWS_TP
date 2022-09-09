b8251eb9ce9c231858f515c4ff8643bf2cb0c01e
EC2 Default User <ec2-user@ip-10-0-0-7.eu-west-3.compute.internal>
2022-09-09 20:24:47 +0000

    [AWS-TP] Template_1 corrige

--- TEMPLATE/Template_1/template1.tf
+++ TEMPLATE/Template_1/template1.tf
@@ -33,30 +33,12 @@ resource "aws_route" "<##INFRA_NAME##>-defroute" {
 	gateway_id = "${aws_internet_gateway.<##INFRA_NAME##>-igw.id}"
 }
 
-resource "aws_network_interface" "<##INFRA_NAME##>-INSTANCE-WEB-IP" {
-	subnet_id = "${aws_subnet.<##INFRA_NAME##>-priv.id}"
-	private_ips = ["10.0.2.40"]
-	
-	tags = {
-		Name = "<##INFRA_NAME##>-INSTANCE-WEB-IP"
-	}
-}
-
-resource "aws_network_interface_sg_attachment" "<##INFRA_NAME##>-INSTANCE-WEB-IP-SG-ATTACH" {
-	security_group_id = "${aws_security_group.<##INFRA_NAME##>-SG-WEB.id}"
-	network_interface_id = "${aws_network_interface.<##INFRA_NAME##>-INSTANCE-WEB-IP.id}"
-}
-
 resource "aws_instance" "<##INFRA_NAME##>-INSTANCE-WEB" {
         key_name = "test_keypair"
         ami = "ami-09e513e9eacab10c1"
 	instance_type = "t2.micro"
 	user_data = "${file("httpd.sh")}"
-
-	network_interface {
-		network_interface_id = "${aws_network_interface.<##INFRA_NAME##>-INSTANCE-WEB-IP.id}"
-		device_index = 0
-	}
+	associate_public_ip_address = "true"	
 
         tags = {
                 Name = "<##INFRA_NAME##>-INSTANCE-WEB"
