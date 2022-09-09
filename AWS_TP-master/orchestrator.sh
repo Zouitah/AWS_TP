9fd4adc025371c0300f04700a649ee36fdfecaed
EC2 Default User <ec2-user@ip-10-0-0-7.eu-west-3.compute.internal>
2022-09-09 17:38:50 +0000

    [AWS-TP] Correction de toute les conneries :)

---
+++ AWS_TP-master/orchestrator.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+INFRA_NAME=$1
+NB_INSTANCE=$2
+TEMPLATE=$3
+
+cp -r TEMPLATE/${TEMPLATE} DEPLOYED/${​INFRA_NAME}​
+
+cd DEPLOYED/${​INFRA_NAME}​
+
+if [ ${NB_INSTANCE} -eq 0 ]
+then
+        for i in {1..${NB_INSTANCE}}
+        do
+            cp instance.tf instance_${i}.tf
+            sed -i "s|<##NUMERO_INSTANCE##>|${i}|g" instance${i}.tf
+            echo "Fichier d'instance ${i} généré"
+        done
+
+        rm -rf instance.tf
+fi
+
+sed -i "s|<##INFRA_NAME##>|${​INFRA_NAME}​|g" *
+
+terraform init
+
+terraform apply -auto-approve
+
+IP=$(cat temp_ip)
+
+NB_SPACE=$(( 16 - $(echo ${​IP}​ | wc -c) ))
+
+SPACES=""
+
+I=0
+
+while [ $I -lt $NB_SPACE ]
+do
+        SPACES=${​SPACES}​" "
+        I=$(( $I + 1 ))
+done
+
+cat << EOF
+
+############################################################
+############################################################
+########                                           #########
+########                                           #########
+########    Web App Available at : ${​IP}​${​SPACES}​#########
+########                                           #########
+########                                           #########
+############################################################
+############################################################
+
+EOF
+
+rm temp_ip
+
+exit
